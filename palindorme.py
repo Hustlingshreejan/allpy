a6c0b302138c5435cfb1b35b30ad7150d8ef69f6
Shreejan Shrestha <Hustlingshreejan@gmail.com>
2020-08-13 12:57:37 +0545

    Version 2nd of Palindorme in which the value less than 10 will get filtered and above value will only sent to test palindrome

--- palindorme.py
+++ palindorme.py
@@ -1,64 +1,38 @@
-# user=int(input("How many times you want to give a input"))
-#
-# for times in range(user):
-#     usertwo=input("Give:")
-#     if usertwo[0]==usertwo[-1]:
-#         print("It itself is the palen")
-#     else:
-#         while(True):
-#             usertwo+=1
-#             if usertwo[0] == usertwo[-1]:
-#                 print("The next  palen is",usertwo)
-#                 break
 
 
-# user=input("Provide the input:")
-# print(user)
-# l=[]
-# for i in range(len(user)):
-#     l.append(user[i])
-# a=l[:]
-# a.reverse()
-# if l ==a:
-#     print("it")
-# else:
-#     new=list(int(i) for i in l)
-#     print(new)
-#
-'''
-Author: Harry
-Date: 15 April 2019
-Purpose: Practice Problem For CodeWithHarry Channel 
-'''
+
+def cal_palindrome(num):
+    num = num+1
+    while not is_palindrome(num):
+        num += 1
+    return num
+
+def is_palindrome(num):
+    return str(num) == str(num)[::-1]
 
 
-def next_palindrome(n):
-    b = n+1
-    while not is_palindrome(b):
-        b += 1
-    return b
-
-def is_palindrome(n):
-    return str(n) == str(n)[::-1]
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
-        n = int(input("Enter the number of test cases\n"))
-        numbers = []
-        for i in range(n):
+        user1 = int(input("Number of input?\n->"))
+        # first empty list to store user's input
+        first_list = []
+        for i in range(user1):
             try:
-                number = int(input("Enter the number:\n"))
-                numbers.append(number)
-
+                user2 = int(input("Your number?\n->"))
+                first_list.append(user2)
             except Exception as e:
-                print("It must be number")
+                print("The input must be integer")
                 exit()
-
-        for i in range(n):
-            print(
-                f"Next palindrome for {numbers[i]} is {next_palindrome(numbers[i])}")
+        print("The numbers you provided to the system to check the palindrome:", first_list,'\nThe value less than 10 is palindrome by itself.')
+        # second empty list to store the final result after calculating palindrome
+        second_list = []
+        for nums in first_list:
+            if nums < 10:
+                second_list.append(nums)
+            else:
+                val = cal_palindrome(nums)
+                second_list.append(val)
+        print(second_list)
     except Exception as e:
-        print("The value must be integer")
+        print('The value must be integer')
         exit()
-
-
